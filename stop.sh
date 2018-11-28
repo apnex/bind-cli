c9358bdf68da4eac221e3319b87cad78931105ca
Andrew Obersnel <apnex@users.noreply.github.com>
2018-11-28 15:18:29 +1100

    Repo cleanup..

--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "-- shutting down running containers --"
-docker rm -f -v $(docker ps -qa) 2>/dev/null
-echo "-- removing untagged containers --"
-docker rmi -f $(docker images -q -f dangling=true) 2>/dev/null
-#docker rmi -f $(docker images -a -q) 2>/dev/null
-echo "-- removing orphaned volumes --"
-docker rm -v $(docker ps -a -q -f status=exited) 2>/dev/null
